86c702c8e6de478fd652cb61b26bf878302f66f4
54 Bridge <54bridgeapp@gmail.com>
2020-01-15 10:07:59 -0600

    added side menu w/ functionality

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector(".backdrop"); //select backdrop class
 var modal = document.querySelector(".modal"); // select modal
 var closeModal = document.querySelector(".modal__action--negative");
 var choosePlan = document.querySelectorAll(".plan button"); //select all buttons nested inside plan class
+var hamBurgerMenu = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
 //loop throuh button to add event listener for a click and set modal and backdrop to block
 choosePlan.forEach(item => {
@@ -12,10 +14,19 @@ choosePlan.forEach(item => {
 });
 
 //remove backdrop on click
-backdrop.addEventListener("click", handleCloseModal);
+backdrop.addEventListener("click", function() {
+  mobileNav.style.display = "none";
+  handleCloseModal();
+});
 //remove modal on click
 closeModal.addEventListener("click", handleCloseModal);
+//show side menu
+hamBurgerMenu.addEventListener("click", function() {
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
+});
 
+//resusable function
 function handleCloseModal() {
   backdrop.style.display = "none";
   modal.style.display = "none";
